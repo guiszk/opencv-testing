2db01ab2672312855eadbe797e7238c61a44a869
guiszk <guiszk@protonmail.com>
2020-12-29 21:43:25 +0000

    Make rename on single folder

--- img_rename.py
+++ img_rename.py
@@ -1,14 +1,15 @@
 import os, sys
- 
-if(len(sys.argv) != 2):
-    print("{0} <path>".format(sys.argv[0]))
+
+if(len(sys.argv) != 2 or not os.path.isdir(sys.argv[1])):
+    print("{0} <folder path>".format(sys.argv[0]))
     sys.exit(1)
- 
-base_dir = os.path.dirname(os.path.abspath(sys.argv[1]))
- 
- a = 0
- 
-for root, dirs, files in os.walk(base_dir):
+
+imgdir = os.path.abspath(sys.argv[1])
+print(imgdir)
+
+a = 0
+
+for root, dirs, files in os.walk(imgdir):
     for file in files:
         if(file.endswith("png") or file.endswith("jpg")):
             a += 1
